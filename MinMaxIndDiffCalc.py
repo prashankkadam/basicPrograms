60e13d0307b5da6d580bd490628a06d20cd39292
prashank kadam <prashankkadam07@gmail.com>
2020-01-02 21:50:28 +0530

    Comments Updated
    Comments Updated

--- MinMaxIndDiffCalc.py
+++ MinMaxIndDiffCalc.py
@@ -11,13 +11,16 @@ version : 1.0
 # Code to calculate the difference in the maximum and minimum indices of
 # all the elements in the list and return the maximum diffence
 
+# Importing the required libraries 
 import ast
 import numpy as np
 
+# Taking input list from the user
 input_list = ast.literal_eval(input())
 unique_list = np.unique(input_list)
 final = []
 
+# Enumerating and finding the differences
 for i in unique_list:
     indices = [m for m, x in enumerate(input_list) if x == i]
     diff = np.max(indices) - np.min(indices)
