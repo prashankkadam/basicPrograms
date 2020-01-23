d318de065fbaece4f2eb1dd4ba66942c2d4eb6ed
prashank kadam <prashankkadam07@gmail.com>
2020-01-23 00:49:32 -0500

    finding factors of a given number
    finding factors of a given number

---
+++ findfactors.py
@@ -0,0 +1,10 @@
+from functools import reduce
+
+
+def fact(n):
+    """Function that returns all the factors of the given number"""
+    return set(reduce(list.__add__,
+                      ([i, n // i] for i in range(1, int(n ** 0.5) + 1) if n % i == 0)))
+
+
+print(fact(10))
