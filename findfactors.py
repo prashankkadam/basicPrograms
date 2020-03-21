fed508eda1a8d5c4b0b7efcedcd101d93ce9e51c
prashank kadam <prashankkadam07@gmail.com>
2020-03-21 18:32:15 -0400

    Update findfactors.py

--- findfactors.py
+++ findfactors.py
@@ -4,7 +4,7 @@ from functools import reduce
 def factor(n):
     """Function that returns all the factors of the given number"""
     return set(reduce(list.__add__,
-                      ([i, n // i] for i in range(1, int(n ** 0.5) + 1) if n % i == 0)))
+                      ([i, n // i] for i in range(1, int(n ** 0.5) + 1) if n % i == 0))) 
 
 
 print(factor(10))
